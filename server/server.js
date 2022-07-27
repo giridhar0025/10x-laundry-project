87fb80866338f26f327d6503f6dafcdd290acf47
Aayu Agrawal <aayuagrawal98@gmail.com>
2022-07-27 18:47:51 +0530

    Usermodel-v1

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const mongoose=require("mongoose")
 const productController=require("./routes/productRoute")
+const userController=require("./routes/userRoute")
+
 const app = express()
 require('dotenv').config();
 app.listen(process.env.PORT, (err) => {
@@ -17,6 +19,7 @@ app.listen(process.env.PORT, (err) => {
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use("/product",productController)
+app.use("/user",userController)
 mongoose.connect(process.env.MONGODB_URL,(err)=>{
     if(!err){
         console.log("Connected to database");
