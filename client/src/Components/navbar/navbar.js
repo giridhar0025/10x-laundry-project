0c4748fe8f4dcf9c413f876deec7a5f1d6ef27a4
giridhar0025 <giridharsonu123@gmail.com>
2022-07-31 17:12:36 +0530

    summary-pop-up-update

--- client/src/Components/navbar/navbar.js
+++ client/src/Components/navbar/navbar.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import './navbar.css'
 
-const Navbar = () => {
-  
+const Navbar = (props) => {
+  console.log(props)
+
+  const token =
+  "eyJhbGciOiJIUzI1NiJ9.cmFodWxAZ21haWwuY29t.jQVg5DcItJ_xFmcuaVXTmf4j5MCFRPhmo1s1gCmqKEs";
+
   return (
     <>
      <div className="navbarContainer">
@@ -11,12 +15,22 @@ const Navbar = () => {
        </div>
        <div className="navbar-btns">
         {
+           token.length ? 
+           <>
+               <button className="n-btns navbarPricing">Pricing</button>
+               <button className="n-btns navbarCareer">Career</button>
+               <button style={{width: "213px", fontSize: "16px;"}} className="n-btns navbarSignIn"><img style={{width: "43px", borderRadius: "50%"}} src="https://www.w3schools.com/howto/img_avatar.png" alt="Avatar" class="avatar"/><span style={{fontSize: "20px", marginLeft: "30px"}}>{props.mainData.name}</span></button>
 
+           </>
+           :
+           <>
+               <button className="n-btns navbarHome">Home</button>
+               <button className="n-btns navbarPricing">Pricing</button>
+               <button className="n-btns navbarCareer">Career</button>
+               <button className="n-btns navbarSignIn">Sign In</button>
+           </>
         }
-         <button className="n-btns navbarHome">Home</button>
-         <button className="n-btns navbarPricing">Pricing</button>
-         <button className="n-btns navbarCareer">Career</button>
-         <button className="n-btns navbarSignIn">Sign In</button>
+        
        </div>
      </div>
     </>
